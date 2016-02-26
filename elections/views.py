65b499dd2c2caea50eb2e2af1838a03329ddba24
Tod <tod@8cruz.com>
2016-02-26 12:00:30 +0900

    9~10 템플릿으로 내용 채우기까지 완료

--- elections/views.py
+++ elections/views.py
@@ -6,10 +6,5 @@ from .models import Candidate
 # Create your views here.
 def index(request):
 	candidates = Candidate.objects.all()
-	str = ''
-	for candidate in candidates:
-		str += "{} 기호{}번({})<BR>".format(candidate.name,
-			candidate.party_number,
-			candidate.area)
-		str += candidate.introduction+"<P>"
-	return HttpResponse(str)
+	context = {'candidates':candidates}
+	return render(request, 'elections/index.html', context)
