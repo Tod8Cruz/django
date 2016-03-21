2ca9685cac985a99eda1a7b7c794814e283533c2
Tod <tod@8cruz.com>
2016-03-21 21:53:49 +0900

    15강 완료.

--- elections/urls.py
+++ elections/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^areas/(?P<area>.+)/$', views.areas),
     url(r'^polls/(?P<poll_id>\d+)/$', views.polls),
-    url(r'^polls/(?P<poll_id>\d+)/result/$', views.result, name = "result"),
 ]
